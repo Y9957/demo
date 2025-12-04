8ad9f976a9d5cadd577c004acd62c71296066c15
minwn <minwn3318@naver.com>
2025-12-04 16:33:57 +0900

    member

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "pass_id", nullable = false, unique = true)
+    private String pass;
+}
